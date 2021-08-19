54357ddf5218ddc1050297fa42d7aa954789c6a0
oogooro <70372978+oogooro@users.noreply.github.com>
2021-08-19 20:05:12 +0200

    Add audio download option

--- index.js
+++ index.js
@@ -8,20 +8,25 @@ const rl = readline.createInterface({
     output: process.stdout
 });
 
-async function cleanup() {
+async function cleanupAudio() {
+    fs.rmSync('temp/output.mp3');
+    fs.rmSync('temp/audio');
+}
+
+async function cleanupVideo() {
     fs.rmSync('temp/output.mp4');
-    fs.rmSync('temp/video.mp4');
-    fs.rmSync('temp/audio.mp3');
+    fs.rmSync('temp/video');
+    fs.rmSync('temp/audio');
 }
 
 function joinAudioVideo(title) {
     console.log('Joining audio + video');
     if (fs.existsSync('temp/output.mp4')) fs.rmSync('temp/output.mp4');
 
-    execSync('ffmpeg -hide_banner -loglevel error -i temp/video.mp4 -i temp/audio.mp3 -c copy -shortest temp/output.mp4');
+    execSync('ffmpeg -hide_banner -loglevel error -i temp/video -i temp/audio -c copy -shortest temp/output.mp4');
     fs.copyFileSync('temp/output.mp4', `downloaded/${title}.mp4`);
 
-    cleanup().catch(err => console.log(`Failed to clean ${err}`));
+    cleanupVideo().catch(err => console.log(`Failed to clean ${err}`));
 
     console.log(`Done. Saved as ${title}.mp4`);
 }
@@ -29,6 +34,7 @@ function joinAudioVideo(title) {
 rl.question('Youtube video URL: ', async (url) => {
 
     if (!fs.existsSync('downloaded')) fs.mkdirSync('downloaded');
+    if (!fs.existsSync('downloaded/audio')) fs.mkdirSync('downloaded/audio');
     if (!fs.existsSync('temp')) fs.mkdirSync('temp');
 
     const videoUrl = url.trim();
@@ -48,33 +54,51 @@ rl.question('Youtube video URL: ', async (url) => {
     resolutions.sort((a, b) => b - a);
     
     console.log(`Avalivable resolutions: ${resolutions.toString().split(',').join(', ')}`);
+    console.log(`Options: audio`);
 
-    rl.question('Select video resolution: ', async (res) => {
+    rl.question('Select video resolution or option: ', async (res) => {
         rl.close();
 
-        const resolution = parseInt(res);
-
-        if (!resolutions.includes(resolution)) return console.log('Bad video resolution');
-
-        console.log('Downloading...');
-
-        const audio = ytdl(videoUrl, { quality: 'highestaudio', filter: 'audioonly' }).pipe(fs.createWriteStream('temp/audio.mp3'));
-        const video = ytdl(videoUrl, { quality: 'highestvideo', filter: format => format.container === 'mp4' && format.height === resolution }).pipe(fs.createWriteStream('temp/video.mp4'));
-        
+        let audioOnly = false;
         let downloadedSomething = false;
 
-        audio.once('finish', () => {
-            console.log('Downloaded audio.');
-            if (downloadedSomething) joinAudioVideo(title);
-            else console.log('Waiting for video to download... \nIf video fail to download try with other resolutions');
-            downloadedSomething = true;
-        });
+        if (res === 'audio') {
+            audioOnly = true;
+            console.log('Downloading...');
 
-        video.once('finish', () => {
-            console.log('Downloaded video.');
-            if (downloadedSomething) joinAudioVideo(title);
-            else console.log('Waiting for audio to download...');
-            downloadedSomething = true;
-        });
+            const audio = ytdl(videoUrl, { quality: 'highestaudio', filter: 'audioonly' }).pipe(fs.createWriteStream('temp/audio'));
+
+            audio.once('finish', () => {
+                console.log('Finished download, encoding...');
+                execSync('ffmpeg -hide_banner -loglevel error -i temp/audio -b:a 192K -vn temp/output.mp3');
+                fs.copyFileSync('temp/output.mp3', `downloaded/audio/${title}.mp3`);
+                cleanupAudio().catch(err => console.log(`Failed to clean ${err}`));
+                console.log(`Done. Saved as ${title}.mp3`);
+            });
+        }
+        else {
+            const resolution = parseInt(res);
+
+            if (!resolutions.includes(resolution)) return console.log('Bad video resolution or option');
+
+            console.log('Starting download...');
+
+            const audio = ytdl(videoUrl, { quality: 'highestaudio', filter: 'audioonly' }).pipe(fs.createWriteStream('temp/audio'));
+            const video = ytdl(videoUrl, { quality: 'highestvideo', filter: format => format.container === 'mp4' && format.height === resolution }).pipe(fs.createWriteStream('temp/video'));
+
+            audio.once('finish', () => {
+                console.log('Downloaded audio.');
+                if (downloadedSomething) joinAudioVideo(title);
+                else console.log('Waiting for video to download... \nIf video fail to download try with other resolutions');
+                downloadedSomething = true;
+            });
+    
+            video.once('finish', () => {
+                console.log('Downloaded video.');
+                if (downloadedSomething) joinAudioVideo(title);
+                else console.log('Waiting for audio to download...');
+                downloadedSomething = true;
+            });
+        }
     });
 });
